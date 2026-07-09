67bfdc8787ab4ae0811b09138faff253331fda52
ushasri999 <modhugumudiushasri@gmail.com>
2026-07-09 08:23:11 +0530

    Read images from Mediastore using ContentResolver

--- app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.example.galleryapp
 
 import android.Manifest
+import android.content.ContentUris
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.example.galleryapp.data.GalleryImage
 import com.example.galleryapp.presentation.GalleryScreen
 import com.example.galleryapp.ui.theme.GalleryAppTheme
 
 class MainActivity : ComponentActivity() {
+    var images: List<GalleryImage>? = null
     private val TAG = "MAIN_ACTIVITY";
     private val permissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { result ->
         if(result) {
+            images = fetchImages()
             Log.d(TAG, "Permission Granted")
+            Log.d(TAG, "images = $images")
         } else {
             Log.d(TAG, "Permission Denied")
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,14 +44,56 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun requestMediaPermissionIfNeeded() {
-        val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    private fun fetchImages(): List<GalleryImage> {
+        val images = mutableListOf<GalleryImage>()
+
+        val projection = arrayOf(MediaStore.Images.Media._ID)
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            null
+        )?.use { cursor ->
+            val columId = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while(cursor.moveToNext()) {
+                val id = cursor.getLong(columId)
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                images.add(
+                    GalleryImage(
+                        id = id,
+                        uri = uri
+                    )
+                )
+            }
+        }
+
+        return images
+    }
+
+    private fun mediaPermissionGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getPermissionForReadingImages(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             Manifest.permission.READ_MEDIA_IMAGES
         } else {
             Manifest.permission.READ_EXTERNAL_STORAGE
         }
+    }
 
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+    private fun requestMediaPermissionIfNeeded() {
+        val permission: String = getPermissionForReadingImages()
+
+        if (mediaPermissionGranted(permission)) {
+            images = fetchImages()
+        } else {
             permissionLauncher.launch(permission)
         }
     }
---
+++ app/src/main/java/com/example/galleryapp/data/GalleryImage.kt
@@ -0,0 +1,8 @@
+package com.example.galleryapp.data
+
+import android.net.Uri
+
+data class GalleryImage (
+    val id: Long,
+    val uri: Uri
+)
